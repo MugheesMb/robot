e3b3f54370f1f40cfb5b822d35d0085b534d88be
mugymb <Mr.Mb@tuta.io>
2020-04-27 01:30:05 +0500

    Updates

---
+++ precache-manifest.c91d6b27a95124256cafca6f0dfde651.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e3b507be04b98498d4376308af2948",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "128bd51f92fd036bf60d",
+    "url": "/robot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6ae7a23044a78e02e351",
+    "url": "/robot/static/css/main.068ca939.chunk.css"
+  },
+  {
+    "revision": "128bd51f92fd036bf60d",
+    "url": "/robot/static/js/2.71ccf3c7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robot/static/js/2.71ccf3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae7a23044a78e02e351",
+    "url": "/robot/static/js/main.a8643c56.chunk.js"
+  },
+  {
+    "revision": "33864a2b2e0ac67d33fb",
+    "url": "/robot/static/js/runtime-main.35336c78.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
